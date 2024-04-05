14d73bf42bb7ccfb6c98a90e395551c2850d3b22
mananvirmani611 <mananvirmani611@gmail.com>
2024-04-05 23:40:53 +0530

    basic setup done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        Basic SignUp Page will be shown here
+      </header>
+    </div>
+  );
+}
+
+export default App;
