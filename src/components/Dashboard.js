7668ebeb4db63487d8811d9e01e9c40ef7f964a5
mananvirmani611 <mananvirmani611@gmail.com>
2024-04-10 22:48:55 +0530

    basic modal added + table replaced

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,19 +3,24 @@ import { Get } from "../services/ThirdPartyUtilityService";
 import constants from "../constants";
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
-import { DataGrid } from '@mui/x-data-grid';
+import Table from '@mui/joy/Table';
+import Button from '@mui/joy/Button';
+import Pagination from '@mui/material/Pagination';
+import Modal from "./Modal";
+
 
 const columns = [ 
-    { field: 'stock', headerName: 'Stock', width: 330 },
-    { field: 'price', headerName: 'Price', width: 700 },
-    { field: '', headerName: 'Buy', width: 130 },
+    { field: 'stock', headerName: 'Stock', width: '500' },
+    { field: 'price', headerName: 'Price', width: '400' },
+    { field: '', headerName: 'Buy', width: '400' },
 ];
-  
 const Dashboard = function(){
     const navigate = useNavigate();
-    const [email, setEmail ]= useState(null);
+    const [email, setEmail ] = useState(null);
     const [stockData, setStockData] = useState(null);
     const [pageNo, setPageNo] = useState(1);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedStockData, setSelectedStockData] = useState({stock : "", price : ""});
     useEffect(() => { 
         if(!localStorage.getItem('login-token')){
             navigate("/login");
@@ -45,12 +50,45 @@ const Dashboard = function(){
     }, [])
     return <div style={{'padding' : '1% 2%'}}>
         < Navbar />
-        {stockData != null && <DataGrid
-            getRowId={(row) => row.stock}
-            rows={stockData}
-            columns={columns}
-        />}
+        {stockData &&
+            <div style={{ 'padding': '2%', 'backgroundColor' : '#F5efea', 'margin' : '1% 0', 'borderRadius' : '10px'}}>
+                <Table sx={{
+                    '& tr td:last-child': {
+                        textAlign: 'center',
+                    },
+                    '& tr th:last-child': {
+                        textAlign: 'center',
+                    },
+                    '& th': {
+                        backgroundColor: 'lightgray', // Set background color to red for all th elements
+                    },
+                    '& tr:nth-of-type(even)': {
+                        backgroundColor: 'white', // Change the background color for odd rows (stripes)
+                    },
+                }} stripe='odd' borderAxis="both" style={{ 'borderRadius': '20px' }}>
+                    <thead>
+                        <tr style={{'backgroundColor' : 'lightgray!important'}}>
+                            <th style={{ width: '40%' }}>Stock Name</th>
+                            <th style={{ width: '40%' }}>Stock Price</th>
+                            <th>Buy Stock</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {stockData.map((row) => (
+                            <tr key={row.stock}>
+                                <td>{row.stock}</td>
+                                <td>{row.price}</td>
+                                <td><Button variant="outlined" onClick={() => {setModalOpen(true); setSelectedStockData({stock : row.stock, price: row.price})}}>Buy </Button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                <Pagination count={5} />
             </div>
+        }
+
+        {modalOpen && <Modal setModalOpen={setModalOpen} stockData={selectedStockData}/>}
+    </div>
 }
 
 export default Dashboard;
---
+++ src/components/Modal.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+const style = `.modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent black overlay */
+    z-index: 1000; /* Ensure the modal is above everything else */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .modal {
+    background-color: white;
+    padding: 20px;
+    width:400px;
+    height:300px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); /* Add a box shadow for a raised effect */
+  }
+  
+  .modal-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+  
+  .modal-content {
+    margin-bottom: 20px;
+  }
+  
+  .modal-footer {
+    display: flex;
+    justify-content: flex-end;
+  }
+  
+  .close-button {
+    background: none;
+    border: none;
+    cursor: pointer;
+  }`;
+const Modal = function ({ setModalOpen, stockData }) {
+  const [password, setPassword] = useState('');
+  const [isOpen, setIsOpen] = useState(true);
+
+  if (!isOpen) return null;
+
+  return <>
+    {isOpen && <div className="modal-overlay">
+      <div className="modal">
+        <div className="modal-header">
+          <h2>{stockData.stock}</h2>
+          <button className="close-button" onClick={() => {
+            setIsOpen(false)
+            setModalOpen(false);
+          }}><CloseIcon /></button>
+        </div>
+        <div className="modal-content">
+          <p>{stockData.price}</p>
+          <div>
+          <button> <RemoveIcon /></button>
+              1
+            <button> <AddIcon /></button>
+          </div>
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="modal-footer">
+          <button onClick={() => console.log('Submit clicked')}>Submit</button>
+        </div>
+      </div>
+    </div>}
+    <style>{style}</style>
+  </>
+}
+
+export default Modal;
