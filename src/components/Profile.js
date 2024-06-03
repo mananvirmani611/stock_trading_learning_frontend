092988f44319c164ec7a0c3925a3e74a3d4adcd7
mananvirmani611 <mananvirmani611@gmail.com>
2024-06-03 22:51:20 +0530

    profile page updation logic changed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -49,7 +49,6 @@ const Profile = function () {
 
         Patch(constants.BASE_API_URL + constants.APIS.SELL_STOCK, reqBody, reqHeaders)
         .then((res) => {
-            console.log(res);
             setModalOpen(false);
 
         })
@@ -68,46 +67,61 @@ const Profile = function () {
         if(!localStorage.getItem('login-token')){
             navigate("/login");
         }
-        console.log(`Bearer ${localStorage.getItem('login-token')}`);
         const response1 = await Get(constants.BASE_API_URL + constants.APIS.VERIFY_TOKEN, {
             Authorization: `Bearer ${localStorage.getItem('login-token')}`,
             Accept: 'application/json'
         })
         setEmail(response1.data.username);
 
-        const response2 = await Get(constants.BASE_API_URL + constants.APIS.USER_DATA + `?email=${response1.data.username}`, {
+        const userDataResponse = await Get(constants.BASE_API_URL + constants.APIS.USER_DATA + `?email=${response1.data.username}`, {
             Authorization: `Bearer ${localStorage.getItem('login-token')}`,
             Accept: 'application/json'
         })
-        setTableData(response2.data);
+        setTableData(userDataResponse.data);
+    }
 
+    async function updateCards(){
+        console.log("update cards called");
+        const response1 = await Get(constants.BASE_API_URL + constants.APIS.VERIFY_TOKEN, {
+            Authorization: `Bearer ${localStorage.getItem('login-token')}`,
+            Accept: 'application/json'
+        })
+        const userDataResponse = await Get(constants.BASE_API_URL + constants.APIS.USER_DATA + `?email=${response1.data.username}`, {
+            Authorization: `Bearer ${localStorage.getItem('login-token')}`,
+            Accept: 'application/json'
+        })
+        const userStockData = userDataResponse.data;
         let stockPricesMaptemp = new Map([]);
-        for(let i = 0; i<response2.data.length; i++){
-            const stockName = response2.data[i].stockName;
+        for(let i = 0; i<userDataResponse.data.length; i++){
+            const stockName = userDataResponse.data[i].stockName;
             if(stockPricesMaptemp.has(stockName))continue;
             const stockPriceData = await Get(constants.BASE_API_URL + constants.APIS.ALL_STOCKS_DATA + `?stock_name=${stockName}`)
             stockPricesMaptemp.set(stockName, stockPriceData.data.data.price);
         }
         setStockPricesMap(stockPricesMaptemp);
-
-        const userStockData = response2.data;
         const balanceResponse = await Get(constants.BASE_API_URL + constants.APIS.CURRENT_BALANCE + `?email=${response1.data.username}`)
-        values[0] = balanceResponse.data.balance;
-        console.log("userrrrrrr stockkkkkkkkkkkkkkkkkkkkkkkkk ", userStockData)
-        values[1] = 0;
-        values[2] = 0;
+       
+        const newvalues = [];
+        newvalues[0] = balanceResponse.data.balance;
+        newvalues[1] = 0;
+        newvalues[2] = 0;
         userStockData.forEach((item) => {
-            values[1] += Number(item.quantity * item.stockPurchasePrice);
-            values[2] += Number(item.quantity * stockPricesMaptemp.get(item.stockName));
+            newvalues[1] += Number(item.quantity * item.stockPurchasePrice);
+            newvalues[2] += Number(item.quantity * stockPricesMaptemp.get(item.stockName));
         })
-        setValues(values);
+        setValues(newvalues);
     }
-
+    
 
     useEffect(() => {
         // eslint-disable-next-line react-hooks/rules-of-hooks
         useEffectF();
-    }, [modalOpen, tabledata] )
+    }, [modalOpen])
+
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        updateCards();
+    }, [tabledata])
   return (
     <div style={{ padding: "1% 2%" }}>
       <Navbar leftText={"Personal Dashboard"} showBalance={false} iconType='Home'/>
